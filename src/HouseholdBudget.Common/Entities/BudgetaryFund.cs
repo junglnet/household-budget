81291cb69df30a0f3dd94855c2ddd8b5a3aa1d18
junglnet <a.derkach@outlook.com>
2019-05-22 21:43:38 +0300

    BL layers added

--- src/HouseholdBudget.Common/Entities/BudgetaryFund.cs
+++ src/HouseholdBudget.Common/Entities/BudgetaryFund.cs
@@ -5,60 +5,62 @@ namespace HouseholdBudget.Common.Entities
 {
     public class BudgetaryFund : DictionaryBase
     {
-        public BudgetaryFund()
-        {
-            Transactions = new List<Transaction>();
-        }
+        //public BudgetaryFund()
+        //{
+        //    Transactions = new List<Transaction>();
+        //}
 
-        public BudgetaryFund(string name)
-        {
-            Name = name;
-            Transactions = new List<Transaction>();
-        }
+        //public BudgetaryFund(string name)
+        //{
+        //    Name = name;
+        //    Transactions = new List<Transaction>();
+        //}
 
-        public BudgetaryFund(string id, string name) : base(id, name)
-        {
-            Transactions = new List<Transaction>();
-        }
+        //public BudgetaryFund(string id, string name) : base(id, name)
+        //{
+        //    Transactions = new List<Transaction>();
+        //}
 
-        /// <summary>
-        /// Добавление транзации в бюджетный фонд
-        /// </summary>
-        /// <param name="transaction"></param>
-        /// <returns>ИД транзации</returns>
-        public string AddTransaction(Transaction transaction)
-        {
-            if (transaction == null) return null;
+        ///// <summary>
+        ///// Добавление транзации в бюджетный фонд
+        ///// </summary>
+        ///// <param name="transaction"></param>
+        ///// <returns>ИД транзации</returns>
+        //public string AddTransaction(Transaction transaction)
+        //{
+        //    if (transaction == null) return null;
 
-            transaction.Id = Guid.NewGuid().ToString("N");
-            Transactions.Add(transaction);
-            Transactions.Sort();
-            return transaction.Id;
-        }
+        //    transaction.Id = Guid.NewGuid().ToString("N");
+        //    Transactions.Add(transaction);
+        //    Transactions.Sort();
+        //    return transaction.Id;
+        //}
 
-        public Transaction FindTransationById(string id)
-        {
-            return new Transaction();
-        }
+        //public Transaction FindTransationById(string id)
+        //{
+        //    return new Transaction();
+        //}
 
-        public List<Transaction> GetTransactions(DateTime startDate, DateTime endDate) =>
-            Transactions.FindAll(d => d.DateTime >= startDate && d.DateTime <= endDate);
+        //public List<Transaction> GetTransactions(DateTime startDate, DateTime endDate) =>
+        //    Transactions.FindAll(d => d.DateTime >= startDate && d.DateTime <= endDate);
 
-        public decimal GetTransactionsSum(DateTime startDate, DateTime endDate)
-        {
-            List<Transaction> tmpTransactions = Transactions.FindAll(d => d.DateTime >= startDate && d.DateTime <= endDate);
+        //public decimal GetTransactionsSum(DateTime startDate, DateTime endDate)
+        //{
+        //    List<Transaction> tmpTransactions = Transactions.FindAll(d => d.DateTime >= startDate && d.DateTime <= endDate);
 
-            if (tmpTransactions.Count == 0) return 0;
-            decimal sum = 0;
-            foreach(Transaction ts in tmpTransactions)
-            {
-                sum = sum + ts.GetOperation();
-            }
-            return sum;
+        //    if (tmpTransactions.Count == 0) return 0;
+        //    decimal sum = 0;
+        //    foreach(Transaction ts in tmpTransactions)
+        //    {
+        //        sum = sum + ts.GetOperation();
+        //    }
+        //    return sum;
 
-        }
+        //}
 
-        public List<Transaction> Transactions { get; set; }
+        //public List<Transaction> Transactions { get; set; }
+
+        public TransactionCollection Transactions { get; set; }
 
     }
 }
---
+++ src/HouseholdBudget.Common/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace HouseholdBudget.Common.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(string id);
+
+        Task<IReadOnlyList<T>> GetByIdsAsync(string[] ids);
+
+        Task<string> AddAsync(T item);
+
+        Task<bool> UpdateAsync(T item);
+
+        Task<bool> DeleteAsync(string id);
+
+        Task<bool> IsExistById(string id);
+    }
+}
